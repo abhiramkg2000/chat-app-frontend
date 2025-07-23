e5bd5b98da001f8f75865e2b6fe2d9509dc0682d
Abhiram Gopalakrishnan <abhiram.g@qburst.com>
2025-07-23 12:23:50 +0530

    added check to change protocol

--- src/hooks/socketClient.ts
+++ src/hooks/socketClient.ts
@@ -3,10 +3,14 @@ import { io, Socket } from "socket.io-client";
 let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
+  const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+
+  console.log("protocol", protocol);
   if (!socket) {
-    socket = io("http://localhost:3001", {
+    socket = io(`${protocol}://localhost:3001`, {
       transports: ["websocket"],
       withCredentials: true,
+      reconnectionAttempts: 5,
       // reconnection: true,
     });
   }
