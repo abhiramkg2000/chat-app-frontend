bb13b7aa77723c4301e7c51d85687110d1360c6c
Abhiram Gopalakrishnan <abhiram.g@qburst.com>
2025-07-23 11:27:56 +0530

    added authentication check on chats page

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,13 +1,51 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 import NavBar from "@/components/navbar/navBar";
 import ChatWindow from "@/modules/chatWindow/chatWindow";
+import AuthenticationCheckText from "@/components/authenticationCheckText/authenticationCheckText";
+
+import { API_URL } from "@/constants/commonConstants";
 
 export default function Chats() {
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetch(`${API_URL}/user/authentication`, {
+          credentials: "include", // Sends the JWT cookie
+        });
+        const data = await res.json();
+
+        if (!data?.authenticated) {
+          router.push("/auth/login");
+        } else {
+          setLoading(false);
+        }
+      } catch (err) {
+        console.error("Error verifying auth:", err);
+        router.push("/auth/login");
+      }
+    };
+
+    checkAuth();
+  }, [router]);
+
   return (
     <>
-      <NavBar />
-      <ChatWindow />
+      {loading ? (
+        <AuthenticationCheckText />
+      ) : (
+        <>
+          <NavBar />
+          <ChatWindow />
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/authenticationCheckText/authenticationCheckText.tsx
@@ -0,0 +1,9 @@
+import "./authenticationCheckText.scss";
+
+export default function AuthenticationCheckText() {
+  return (
+    <div className="authentication-check">
+      <p>Checking authentication...</p>
+    </div>
+  );
+}
