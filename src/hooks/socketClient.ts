74bc86b8aea867599be6eb93addc793e6ebf878f
Abhiram Gopalakrishnan <abhiram.g@qburst.com>
2025-07-23 12:34:31 +0530

    changed getSocket function

--- src/hooks/socketClient.ts
+++ src/hooks/socketClient.ts
@@ -1,13 +1,15 @@
 import { io, Socket } from "socket.io-client";
 
+import { API_URL } from "@/constants/commonConstants";
+
 let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
-  const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+  // const protocol = window.location.protocol === "https:" ? "wss" : "ws";
 
-  console.log("protocol", protocol);
+  // console.log("protocol", protocol);
   if (!socket) {
-    socket = io(`${protocol}://localhost:3001`, {
+    socket = io(`${API_URL}`, {
       transports: ["websocket"],
       withCredentials: true,
       reconnectionAttempts: 5,
