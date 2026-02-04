fcfd2056c9aeb2c0170c23c4bafded6e310f1f33
Abhiram Gopalakrishnan <abhikgram2000@gmail.com>
2026-02-04 12:54:56 +0530

    changed the authentication check message

--- src/components/authenticationCheckText/authenticationCheckText.tsx
+++ src/components/authenticationCheckText/authenticationCheckText.tsx
@@ -3,7 +3,7 @@ import "./authenticationCheckText.scss";
 export default function AuthenticationCheckText() {
   return (
     <div className="authentication-check">
-      <p>Checking authentication...</p>
+      <p>Checking authentication and redirecting...</p>
     </div>
   );
 }
